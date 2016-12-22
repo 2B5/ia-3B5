7d5705e7d34ad232dba8a2765a6f933e97185511
Marian FX <marian.focsa@outlook.com>
2016-12-22 04:33:37 +0200

    Updated README with Logs and Added Talks Samples.

--- module1/app/bot-core.js
+++ module1/app/bot-core.js
@@ -2,16 +2,32 @@ var Requester = require("./requester.js");
 const AnswerBot = require("./data.js");
 const answerBot = new AnswerBot()
 const baseM2Address = 'https://3668928a.ngrok.io';
+const preprocAddress = 'https://ia-module3.herokuapp.com/process/'
 class BotCore{
 
   handle_message(input_message, cb){
-    var m2requester = new Requester(baseM2Address + '?question=' + input_message);
+
+    var m2preprocess  = new Requester(preprocAddress + encodeURIComponent(input_message));
+    var me = this;
+
+    // step 1 - correct text
+    m2preprocess.doRequest(false, function(json){
+        console.log(json)
+
+        // inner step - process types of words
+
+        // step last - check answers
+        me.process_message(json, cb)
+    });
+  }
+
+  process_message(json, cb){
+    //facem magia cu json-ul primit de la bilan
+    var m2requester = new Requester(baseM2Address + '?question=' + json.corrected);
     m2requester.doRequest(true, function(text){
       cb(text);
     });
-    //cb("DA");
   }
-  
 }
 
 module.exports = BotCore
